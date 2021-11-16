a1b487b73bdbe022c5ceed134336bbc80266f9e4
PQCraft <0456523@gmail.com>
2021-11-16 07:11:58 -0500

    Build 2021111502

--- resources/common/shaders/fragment.glsl
+++ resources/common/shaders/fragment.glsl
@@ -6,9 +6,13 @@ in vec2 TexCoord;
 
 // texture samplers
 uniform sampler2D TexData;
+uniform int TexData;
 
 void main()
 {
-	// linearly interpolate between both textures (80% container, 20% awesomeface)
-	FragColor = texture(TexData, TexCoord) * vec4(CurColor, 1.0);
+    if (HasTex) {
+    	FragColor = texture(TexData, TexCoord) * vec4(CurColor, 1.0);
+    } else {
+    	FragColor = vec4(CurColor, 1.0);
+    }
 }
